276fd87a55cfc15623be6cde240a072c25b309e8
Rivai20 <muhammadrivailie20@gmail.com>
2025-10-28 00:08:04 +0900

    UTS

--- UTS/README.md
+++
@@ -1,3 +0,0 @@
-Nama : Muhammad Rivai lie
-NPM : 07352311052
-LINK YOUTUBE : https://www.youtube.com/live/O3OL4uNSpio?si=lLgmbsIYXxmHOrcp
--- UTS/test/tes_uts_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tes_uts/tes_uts.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
